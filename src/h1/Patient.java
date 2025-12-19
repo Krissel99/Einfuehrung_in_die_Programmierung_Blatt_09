2183059ea7c0229c8e9a5de387c4a1019d1d3514
kkallmey <kkallmey@smail.uni-koeln.de>
2025-12-19 20:50:15 +0100

    Aufgaben auf entsprechend der Änderungen der Objektattribute von private auf public angepasst + die transferPassengers Methode leicht verändert

--- src/h1/Patient.java
+++ src/h1/Patient.java
@@ -2,24 +2,24 @@ package h1;
 
 
 public class Patient {
-    private String name;
-    private int prio;
+    public String name;
+    public int prio;
 
     public Patient(String name, int prio) {
         this.name = name;
         this.prio = prio;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getPrio() {
-        return prio;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Patient: %-10s | Priorität: %2d", name, prio);
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getPrio() {
+//        return prio;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return String.format("Patient: %-10s | Priorität: %2d", name, prio);
+//    }
 }
--- src/h1/PrioListe.java
+++ src/h1/PrioListe.java
@@ -3,37 +3,29 @@ package h1;
 import java.util.ArrayList;
 
 public class PrioListe {
-    private ArrayList<Patient> myList = new ArrayList<>();
+    public ArrayList<Patient> myList = new ArrayList<>();
 
     public void addPatient(Patient p) {
-        if (myList.isEmpty() || p.getPrio() > myList.getLast().getPrio()) {
+        if (myList.isEmpty() || p.prio > myList.getLast().prio) {
             myList.add(p);
             return;
         }
 
         for (Patient q : myList) {
-            if (p.getPrio() < q.getPrio()) {
+            if (p.prio < q.prio) {
                 myList.add(myList.indexOf(q), p);
                 return;
             }
         }
-//        myList.sort(Comparator.comparing(Patient::getPrio));
     }
 
     public Patient getNextPatient() {
         Patient first = myList.getFirst();
-        myList.remove(myList.getFirst());
+        myList.removeFirst();
         return first;
     }
 
     public int getPosition(Patient p) {
         return myList.indexOf(p);
-    }
-
-    public void printList() {
-        System.out.println("Priorität | Patient");
-        for (Patient p : myList) {
-            System.out.printf("%-10d  %-10s %n", p.getPrio(), p.getName());
-        }
     }
 }
